bb04b3dde504476fc796a6876aacc1847151de67
chunxi <51769117+chunxi-alpc@users.noreply.github.com>
2019-08-13 11:54:25 +0800

    Add files via upload

--- halma.py
+++ halma.py
@@ -1,50 +1,44 @@
-import pygame
+from pygame import*
 import sys
-
-def init():
-    pygame.init()  # 初始化pygame
-    size = width, height = 860, 750  # 设置窗口大小
-    screen = pygame.display.set_mode(size)  # 显示窗口
-    background = pygame.image.load('./image/board.png')
-    b=[]
-    r=[]
-    for i in range(10):
-        b.append(pygame.image.load('./image/蓝棋'+str(i)+'.png'))
-    for i in range(10):
-        r.append(pygame.image.load('./image/红棋'+str(i)+'.png'))
-    sound_background = pygame.mixer.Sound("./sound/background.wav")
-    select_image = pygame.image.load("./image/select.png").convert_alpha()
-    net_mode_image = pygame.image.load("./image/net_mode.png").convert_alpha()
-    ai_mode_image = pygame.image.load("./image/ai_mode.png").convert_alpha()
-    repent_image = pygame.image.load("./image/repent.png").convert_alpha()
-    restart_image = pygame.image.load("./image/restart.png").convert_alpha()
-    quit_image = pygame.image.load("./image/quit.png").convert_alpha()
-    
-    sound_move = pygame.mixer.Sound("./sound/move.wav")
-    area= background.get_rect()  # 获取矩形区域
-    while True:  # 死循环确保窗口一直显示
-        for event in pygame.event.get():  # 遍历所有事件
-            if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
-                sys.exit()
-        screen.blit(background, area)
-        screen.blit(select_image,area)
-        dd=160
-        ss=28
-        h=145
-        screen.blit(net_mode_image, (ss, h+3))
-        screen.blit(ai_mode_image, (ss+dd ,h))
-        screen.blit(repent_image, (ss+dd*2, h))
-        screen.blit(restart_image, (ss+dd*3,h))
-        screen.blit(quit_image, (ss+dd*4,h+7))
-       
-        screen.blit(b[0],(5,450))
-        
-        screen.blit(r[0],(800,450))
-        
-        sound_background.play()
-        
-        pygame.display.flip()  # 更新全部显示
-    pygame.quit()  # 退出pygame
-
+from setting import*
+#from netmode import*
+#from local import*
 if __name__ == '__main__':
-    init()
+    setting()
+    while True:  # 死循环确保窗口一直显示
+        for e in event.get():  # 遍历所有事件
+            if e.type == QUIT:  # 如果单击关闭窗口，则退出
+                sys.exit()
+            # 按Esc则退出游戏
+            if e.type == KEYDOWN:
+                if e.key == K_ESCAPE:
+                    exit()
+            if e.type == MOUSEBUTTONDOWN:
+                x,y = mouse.get_pos()
+                dd=160
+                ss=28
+                h=145
+                xx=150
+                yy=50
+                if x in range(ss,ss+xx) and y in range(h+3,h+3+yy) :
+                    print('网络模式')
+                    #net_mode()
+                elif x in range(ss+dd,ss+dd+xx) and y in range(h,h+yy) :
+                    print('人机模式')
+                   # local()
+                elif x in range(ss+dd*2,ss+dd*2+xx) and y in range(h,h+yy) :
+                    print('悔棋')
+                elif x in range(ss+dd*3,ss+dd*3+xx) and y in range(h,h+yy) :
+                    print('重新开始')
+                    setting()
+                elif x in range(ss+dd*4,ss+dd*4+xx) and y in range(h,h+yy) :
+                    print('游戏结束')
+                    
+            pressed_keys = key.get_pressed()
+            # 这里获取鼠标的按键情况
+            pressed_mouse = mouse.get_pressed()
+            
+        display.flip()  # 更新全部显示
+
+    quit()  # 退出pygame
+
