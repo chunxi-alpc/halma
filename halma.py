0fa1e4f001a6b30766ee6b0793054a20a48b933b
chunxi <51769117+chunxi-alpc@users.noreply.github.com>
2019-08-14 10:25:47 +0800

    Add files via upload

--- halma.py
+++ halma.py
@@ -3,7 +3,7 @@ import sys
 from math import *
 
 role='blue'
-
+selected=(1000,1000)
 chess_pos =[[0]*15 for i in range(15)]
 chess0 =[[] for i in range(8)]
 dd=65
@@ -56,37 +56,34 @@ flag=[[-2, -2, -2, -2, -2, -2, -2, -1, -2, -2, -2, -2, -2, -2, -2],
 [-2, -2, -2, -2, -2, -1, -2, -1, -2, -1, -2, -2, -2, -2, -2],
 [-2, -2, -2, -2, -2, -2, -1, -2, -1, -2, -2, -2, -2, -2, -2],
 [-2, -2, -2, -2, -2, -2, -2, -1, -2, -2, -2, -2, -2, -2, -2]]
-
+b=[]
+r=[]
 init()
 size = width, height = 860, 750  # 设置窗口大小
 screen = display.set_mode(size)  # 显示窗口
 select_image = image.load("./image/select.png").convert_alpha()
-
-def setting():
-    
-    background = image.load('./image/board.png')
-    b=[]
-    r=[]
-    for i in range(10):
-        b.append(image.load('./image/蓝棋'+str(i)+'.png'))
-    for i in range(10):
-        r.append(image.load('./image/红棋'+str(i)+'.png'))
-    area=b[0].get_rect()
-    net_mode_image = image.load("./image/net_mode.png").convert_alpha()
-    ai_mode_image = image.load("./image/ai_mode.png").convert_alpha()
-    repent_image = image.load("./image/repent.png").convert_alpha()
-    restart_image = image.load("./image/restart.png").convert_alpha()
-    quit_image = image.load("./image/quit.png").convert_alpha()
-    sound_background = mixer.Sound("./sound/background.wav")
-    sound_move = mixer.Sound("./sound/move.wav")
-    area= background.get_rect()  # 获取矩形区域
-    screen.blit(background, area)
-    display.set_caption("国际跳棋")
-
-    #棋子初始坐标
+background = image.load('./image/board.png')
+for i in range(10):
+    b.append(image.load('./image/蓝棋'+str(i)+'.png'))
+for i in range(10):
+    r.append(image.load('./image/红棋'+str(i)+'.png'))
+area=b[0].get_rect()
+net_mode_image = image.load("./image/net_mode.png").convert_alpha()
+ai_mode_image = image.load("./image/ai_mode.png").convert_alpha()
+repent_image = image.load("./image/repent.png").convert_alpha()
+restart_image = image.load("./image/restart.png").convert_alpha()
+quit_image = image.load("./image/quit.png").convert_alpha()
+sound_background = mixer.Sound("./sound/background.wav")
+sound_move = mixer.Sound("./sound/move.wav")
+area= background.get_rect()  # 获取矩形区域
+def draw():
     global chess_pos
     global flag
-    #screen.blit(select_image,chess[5][5])
+    screen.blit(background, area)
+    if selected != (1000,1000):
+        screen.blit(select_image,chess_pos[selected[0]][selected[1]])
+    display.set_caption("国际跳棋")
+    #棋子初始坐标
     dd=160
     ss=28
     h=145
@@ -95,7 +92,6 @@ def setting():
     screen.blit(repent_image, (ss+dd*2, h))
     screen.blit(restart_image, (ss+dd*3,h))
     screen.blit(quit_image, (ss+dd*4,h+7))
-
     for i in range(15):
         for j in range(15):
             if flag[i][j]>-1:
@@ -103,12 +99,8 @@ def setting():
                     screen.blit(b[flag[i][j]],chess_pos[i][j])
                 else:
                     screen.blit(r[flag[i][j]-10],chess_pos[i][j])
-    '''
-    for i in range(10):
-        screen.blit(b[i],xy_b[i])
-        screen.blit(r[i],xy_r[i])
-    '''
-    #sound_background.play()
+
+#sound_background.play()
 
 def is_chess_clicked(x,y):
     d=54
@@ -119,12 +111,34 @@ def is_chess_clicked(x,y):
             if flag[i][j]>-1:
                 if (role=='blue' and flag[i][j]<10)or(role=='red' and flag[i][j]>9):
                     if x>chess_pos[i][j][0] and x<chess_pos[i][j][0]+d and y>chess_pos[i][j][1] and y<chess_pos[i][j][1]+d :
-                        return chess_pos[i][j]
+                        return i,j
     return None
-#def removable(x,y):
-    
+
+def is_space_clicked(x,y,select_chess):
+    global chess_pos
+    global flag
+    d=54
+    dd=90
+    #被移动棋子的坐标
+    xx,yy=select_chess
+    if (x-chess_pos[xx][yy][0])*(x-chess_pos[xx][yy][0])+(y-chess_pos[xx][yy][1])*(y-chess_pos[xx][yy][1])>dd*dd:
+        return None
+   
+    dxy=[(xx-1,yy-1),(xx,yy-2),(xx+1,yy-1),(xx-1,yy+1),(xx,yy+2),(xx+1,yy+1)]
+
+    for xy in dxy:
+        i=xy[0]
+        j=xy[1]
+        if flag[i][j]==-1:
+            if x>chess_pos[i][j][0] and x<chess_pos[i][j][0]+d and y>chess_pos[i][j][1] and y<chess_pos[i][j][1]+d :        
+                return i,j
+    return None
+
+
 if __name__ == '__main__':
-    setting()
+
+    select_chess = None
+    draw()
     while True:  # 死循环确保窗口一直显示
         for e in event.get():  # 遍历所有事件
             if e.type == QUIT:  # 如果单击关闭窗口，则退出
@@ -132,38 +146,54 @@ if __name__ == '__main__':
             # 按Esc则退出游戏
             if e.type == KEYDOWN:
                 if e.key == K_ESCAPE:
-                    exit()
+                    sys.exit()
+                    
             if e.type == MOUSEBUTTONDOWN:
                 x,y = mouse.get_pos()
                 selected = is_chess_clicked(x,y)
                 if selected is not None:
                         # 本次点击点击到了棋子
-                        screen.blit(select_image,selected)
+                        print(selected)
+                        
+                        select_chess=selected
                 else:
-                    dd=160
-                    ss=28
-                    h=145
-                    xx=150
-                    yy=50
-                    if x in range(ss,ss+xx) and y in range(h+3,h+3+yy) :
-                        print('网络模式')
-                        #net_mode()
-                    elif x in range(ss+dd,ss+dd+xx) and y in range(h,h+yy) :
-                        print('人机模式')
-                       # local()
-                    elif x in range(ss+dd*2,ss+dd*2+xx) and y in range(h,h+yy) :
-                        print('悔棋')
-                    elif x in range(ss+dd*3,ss+dd*3+xx) and y in range(h,h+yy) :
-                        print('重新开始')
-                        setting()
-                    elif x in range(ss+dd*4,ss+dd*4+xx) and y in range(h,h+yy) :
-                        print('游戏结束')
-                    
-            pressed_keys = key.get_pressed()
-            # 这里获取鼠标的按键情况
-            pressed_mouse = mouse.get_pressed()
-            
-        display.flip()  # 更新全部显示
+                    selected=is_space_clicked(x,y,select_chess)
+    
+                    if selected is not None:
+                        # 本次点击点击到了可移动的空格
+                        i,j=selected
+                        ii,jj=select_chess
+                        print(i,j,ii,jj)
+                        if(role=='blue'):
+                            flag[i][j]=flag[ii][jj]
+                            flag[ii][jj]=-1
+                            '''
+                            tmp=chess_pos[i][j]
+                            chess_pos[i][j]=chess_pos[ii][jj]
+                            chess_pos[ii][jj]=tmp
+                            '''
+                            select_chess = None
+                    else:
+                        dd=160
+                        ss=28
+                        h=145
+                        xx=150
+                        yy=50
+                        if x in range(ss,ss+xx) and y in range(h+3,h+3+yy) :
+                            print('网络模式')
+                            #net_mode()
+                        elif x in range(ss+dd,ss+dd+xx) and y in range(h,h+yy) :
+                            print('人机模式')
+                           # local()
+                        elif x in range(ss+dd*2,ss+dd*2+xx) and y in range(h,h+yy) :
+                            print('悔棋')
+                        elif x in range(ss+dd*3,ss+dd*3+xx) and y in range(h,h+yy) :
+                            print('重新开始')
+                            setting()
+                        elif x in range(ss+dd*4,ss+dd*4+xx) and y in range(h,h+yy) :
+                            print('游戏结束')
+            draw()
+            display.flip()  # 更新全部显示
 
     quit()  # 退出pygame
 
